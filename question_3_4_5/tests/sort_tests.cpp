20140983ecf8f2f2fa9f27aa0cd23f7ee114b3e0
Tegan Lamoureux <tegan.lamoureux@sensio.com>
2026-02-07 10:34:08 +0100

    Add more sort test edge cases.

--- question_3_4_5/tests/sort_tests.cpp
+++ question_3_4_5/tests/sort_tests.cpp
@@ -52,7 +52,7 @@ TEST(SortTests, Sort) {
 		if (previousYear) { // skip first
 			currentYear = currentCar.getYear();
 
-			std:: cout << "Checking that `" << previousYear << "` > `" 
+			std:: cout << "Checking that `" << previousYear << "` >= `" 
 					   << currentYear << "`." << std::endl;
 			ASSERT_GE(previousYear, currentYear);
 		}
@@ -73,7 +73,7 @@ TEST(SortTests, Sort) {
 		if (previousYear) { // skip first
 			currentYear = currentCar.getYear();
 			
-			std:: cout << "Checking that `" << previousYear << "` > `" 
+			std:: cout << "Checking that `" << previousYear << "` >= `" 
 					   << currentYear << "`." << std::endl;
 			ASSERT_GE(previousYear, currentYear);
 		}
@@ -82,6 +82,122 @@ TEST(SortTests, Sort) {
 	std::cout << std::endl;
 }
 
-// check list duplicates
+// Okay maybe a few more tests.. Check edge cases
+TEST(SortTests, SortListFullOfDuplicates) {
+	std::vector<Syn::Car> cars1 {
+		Syn::Car("Mazda", "RX7", 1995),
+		Syn::Car("Mazda", "RX7", 1995),
+		Syn::Car("Mazda", "RX7", 1995),
+		Syn::Car("Mazda", "RX7", 1995),
+		Syn::Car("Mazda", "RX7", 1995),
+	};
 
+	Syn::LinkedList list1(5);
+	for (auto car : cars1) {
+		ASSERT_TRUE(list1.appendFront(car));
+	}
+	ASSERT_EQ(5, list1.size());
 
+	Syn::LinkedList sortedList(5);
+	list1.sortDescending(sortedList);
+
+	uint32_t previousYear = 0;
+	uint32_t currentYear  = 0;
+
+	for (auto currentCar : sortedList) {
+		if (previousYear) { // skip first
+			currentYear = currentCar.getYear();
+			ASSERT_GE(previousYear, currentYear);
+		}
+		previousYear = currentCar.getYear();
+	}
+}
+
+TEST(SortTests, SortListFullOfBadData) {
+	std::vector<Syn::Car> cars1 {
+		Syn::Car("", "", 0),
+		Syn::Car("", "", 0),
+		Syn::Car("", "", 0),
+		Syn::Car("", "", 0),
+		Syn::Car("", "", 0),
+	};
+
+	Syn::LinkedList list1(5);
+	for (auto car : cars1) {
+		ASSERT_TRUE(list1.appendFront(car));
+	}
+	ASSERT_EQ(5, list1.size());
+
+	Syn::LinkedList sortedList(5);
+	list1.sortDescending(sortedList);
+
+	uint32_t previousYear = 0;
+	uint32_t currentYear  = 0;
+
+	for (auto currentCar : sortedList) {
+		if (previousYear) { // skip first
+			currentYear = currentCar.getYear();
+			ASSERT_GE(previousYear, currentYear);
+		}
+		previousYear = currentCar.getYear();
+	}
+}
+
+TEST(SortTests, SortEmptyList) {
+	Syn::LinkedList list1;
+
+	ASSERT_EQ(0, list1.size());
+
+	Syn::LinkedList sortedList;
+	list1.sortDescending(sortedList);
+
+	ASSERT_EQ(0, sortedList.size());
+}
+
+TEST(SortTests, SortListOf1) {
+	Syn::LinkedList list1(1);
+	ASSERT_TRUE(list1.appendFront(Syn::Car("Mazda", "RX7", 1995)));
+	ASSERT_EQ(1, list1.size());
+
+	Syn::LinkedList sortedList(1);
+	list1.sortDescending(sortedList);
+
+	ASSERT_EQ(1, sortedList.size());
+}
+
+TEST(SortTests, SortListOf2) {
+	Syn::LinkedList list1(2);
+	ASSERT_TRUE(list1.appendFront(Syn::Car("Mazda", "RX7", 1995)));
+	ASSERT_TRUE(list1.appendFront(Syn::Car("Honda", "Civic", 1997)));
+	ASSERT_EQ(2, list1.size());
+
+	Syn::LinkedList sortedList(2);
+	list1.sortDescending(sortedList);
+
+	ASSERT_EQ(2, sortedList.size());
+
+	auto car1 = sortedList.begin();
+	auto car2 = ++sortedList.begin();
+
+	ASSERT_GE((*car1).getYear(), (*car2).getYear());
+}
+
+TEST(SortTests, SortListOf3) {
+	Syn::LinkedList list1(3);
+	ASSERT_TRUE(list1.appendFront(Syn::Car("Mazda", "RX7", 1995)));
+	ASSERT_TRUE(list1.appendFront(Syn::Car("Toyota", "GR Yaris", 2025)));
+	ASSERT_TRUE(list1.appendFront(Syn::Car("Honda", "Civic", 1997)));
+	ASSERT_EQ(3, list1.size());
+
+	Syn::LinkedList sortedList(3);
+	list1.sortDescending(sortedList);
+
+	ASSERT_EQ(3, sortedList.size());
+
+	auto car1 = sortedList.begin();
+	auto car2 = ++sortedList.begin();
+	auto car3 = ++(++sortedList.begin());
+
+	ASSERT_GE((*car1).getYear(), (*car2).getYear());
+	ASSERT_GE((*car2).getYear(), (*car3).getYear());
+}
